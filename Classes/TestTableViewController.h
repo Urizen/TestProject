0e00725d7096ac7145ef43882ae245d26a034c6b
Urizen <urizen@silver-rain.com>
2009-10-18 14:32:10 +0100

    Added the Table functionality for the second tab.
    The data has not been hooked up yet.

---
+++ Classes/TestTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  TestTableViewController.h
+//  TestProject
+//
+//  Created by Boris Dapaah on 18/10/2009.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TestTableViewController : UITableViewController {
+
+}
+
+@end
